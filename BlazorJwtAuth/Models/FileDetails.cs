70a4b91aebee5c2c9957d650a3025378a7036526
milag-git <guntaka.m.s@gmail.com>
2024-04-22 23:51:00 -0400

    Initial commit

---
+++ BlazorJwtAuth/Models/FileDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorJwtAuth.Models
+{
+  
+     
+        public class FileDetails
+        {
+            public string? FileNumber { get; set; }
+
+            public string? ProjectInfo { get; set; }
+
+        }
+    
+}
---
+++ BlazorJwtAuth/Program.cs
@@ -0,0 +1,19 @@
+using Blazored.LocalStorage;
+using BlazorJwtAuth;
+using BlazorJwtAuth.AuthProviders;
+using BlazorJwtAuth.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://interrawebapiapp.azurewebsites.net") });
+builder.Services.AddScoped<AuthenticationStateProvider, AuthProvider>();
+builder.Services.AddScoped<IAuthenticationService,AuthenticationService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+await builder.Build().RunAsync();
